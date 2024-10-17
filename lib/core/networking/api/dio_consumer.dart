a277cdf6bef2430b21c9356fb7934c627dd6be2e
mostafa-sabry <engmostafasabryhassan@gmail.com>
2024-10-17 21:04:36 +0300

    file Api

---
+++ lib/core/networking/api/dio_consumer.dart
@@ -0,0 +1,81 @@
+import 'package:dio/dio.dart';
+import 'package:online_store/core/networking/api/api_consumer.dart';
+import 'package:online_store/core/networking/api/api_interceptors.dart';
+import 'package:online_store/core/networking/api/end_points.dart';
+
+class DioConsumer extends ApiConsumer {
+  final Dio dio;
+
+  DioConsumer({required this.dio}) {
+    dio.options.baseUrl = EndPoints.baseUrl;
+    dio.interceptors.add(ApiInterceptors());
+    dio.interceptors.add(LogInterceptor(
+      request: true,
+      requestHeader: true,
+      requestBody: true,
+      responseHeader: true,
+      responseBody: true,
+      error: true,
+    ));
+  }
+
+  @override
+  Future delete(String path,
+      {Object? data, Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response = await dio.delete(
+        path,
+        data: data,
+        queryParameters: queryParameters,
+      );
+      return response.data;
+    } on DioException catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Future get(String path,
+      {Object? data, Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response = await dio.get(
+        path,
+        data: data,
+        queryParameters: queryParameters,
+      );
+      return response.data;
+    } on DioException catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Future post(String path,
+      {Object? data, Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response = await dio.post(
+        path,
+        data: data,
+        queryParameters: queryParameters,
+      );
+      return response.data;
+    } on DioException catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Future put(String path,
+      {Object? data, Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response = await dio.put(
+        path,
+        data: data,
+        queryParameters: queryParameters,
+      );
+      return response.data;
+    } on DioException catch (e) {
+      print(e);
+    }
+  }
+}
--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,7 +1,6 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:online_store/core/helpers/spacing.dart';
+import '../widget/banner_widget.dart';
 import '../widget/categoris_widget.dart';
 import '../widget/category_name_widget.dart';
 import '../widget/heder_widget.dart';
@@ -35,39 +34,3 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-
-class BannersWidget extends StatelessWidget {
-  const BannersWidget({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return CarouselSlider(
-      options: CarouselOptions(height: 200.0),
-      items: [1, 2, 3, 4, 5].map((i) {
-        return Builder(
-          builder: (BuildContext context) {
-            return Container(
-              width: MediaQuery.of(context).size.width,
-              margin: const EdgeInsets.symmetric(horizontal: 5.0),
-              child: Text(
-                'text $i',
-                style: const TextStyle(fontSize: 16.0),
-              ),
-            );
-          },
-        );
-      }).toList(),
-    );
-  }
-}
-
-
-// Container(
-    //   width: double.infinity,
-    //   height: 130.h,
-    //   decoration: const BoxDecoration(
-    //     color: Colors.blueGrey,
-    //   ),
-    // );
